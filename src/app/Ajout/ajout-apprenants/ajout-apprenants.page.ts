996e3527ab9813939d2b18de53f0800a3175fa78
Diakalia Konaté <diakisskonate5@gmail.com>
2021-12-20 10:23:25 +0000

    premier commit

---
+++ src/app/Ajout/ajout-apprenants/ajout-apprenants.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajout-apprenants',
+  templateUrl: './ajout-apprenants.page.html',
+  styleUrls: ['./ajout-apprenants.page.scss'],
+})
+export class AjoutApprenantsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
